199b0ffc71dda2ecc9503f53d6698bee14e122a3
Thiago Bertolino <thiagopbertolino@gmail.com>
2021-05-18 16:35:07 -0300

    Teste

--- ContaBancaria/ContaBancaria.php
+++ ContaBancaria/ContaBancaria.php
@@ -6,7 +6,7 @@ class ContaBancaria
     // public - private - protected
 
     public $banco;
-    public $nomeTitular = 'Thiago';
+    public $nomeTitular = 'Thiago Bertolino';
     public $numeroAgencia;
     public $numberoConta;
     public $saldo;
